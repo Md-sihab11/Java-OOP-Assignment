769e41500199e9a49d6cca6d66c95910a344069e
MD Shakib <mdshakib22@gmail.com>
2025-08-24 00:13:28 +0600

    Start Java OOP assignment

---
+++ Default_constructor.java
@@ -0,0 +1,11 @@
+public class Default_constructor {
+    Default_constructor()
+        {
+            System.out.println("DIU");
+        }
+    public static void main(String[] args) {
+        Default_constructor ob = new Default_constructor();
+
+    }
+    
+}
