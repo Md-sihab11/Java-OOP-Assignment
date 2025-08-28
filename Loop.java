9db74f1846d86aa998ef58e12f22228166cd8c00
MD Shakib <mdsihabsharar22@gmail.com>
2025-08-29 00:18:52 +0600

    Second commit

--- Loop.java
+++ Loop.java
@@ -3,23 +3,18 @@ public class Loop {
    public Loop() {
    }
 
-   void m(int... var1) {
-      int var2 = 0;
-      int[] var3 = var1;
-      int var4 = var1.length;
-
-      for(int var5 = 0; var5 < var4; ++var5) {
-         int var6 = var3[var5];
-         var2 += var6;
+   void m(int ...a) {
+      int sum = 0;
+      for(int i:a){
+         sum+=i;
       }
-
-      System.out.println("Results: " + var2);
+      System.out.println("Results: " + sum);
    }
 
-   public static void main(String[] var0) {
-      Loop var1 = new Loop();
-      var1.m(10);
-      var1.m(10, 20);
-      var1.m(10, 20, 30);
+   public static void main(String[] args) {
+      Loop ob = new Loop();
+      ob.m(10);
+      ob.m(10, 20);
+      ob.m(10, 20, 30);
    }
 }
